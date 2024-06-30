9f8053c9aec2b7eece02fc3d60cac64a4a476f6f
canepari-luke <can20006@byui.edu>
2024-06-29 21:23:01 -0600

    Added a contact us page and java script.

---
+++ wwr/contactus.js
@@ -0,0 +1,6 @@
+// This script file can be used for various interactive features on your site
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    // Add your JavaScript code here
+});
